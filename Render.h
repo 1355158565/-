ddaccb7c87210045adf0fecba57e3f8c597bba56
1355158565 <60255109+1355158565@users.noreply.github.com>
2020-05-04 16:53:58 +0800

    Create Render.h

---
+++ Render.h
@@ -0,0 +1,11 @@
+class Render{
+  public:
+    void inter_face();//初始化界面
+    void init_dia();//初始化方块信息
+    void gotoxy();//移动光标
+    void hidden_cursor();//隐藏光标
+    void draw_kong(); //画方块
+    void grade;//显示当前分数
+    void max;//显示最高记录
+  private:
+  };
