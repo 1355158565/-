72e248fbec733d0f9bd5c23e76fea0753772431b
1355158565 <60255109+1355158565@users.noreply.github.com>
2020-05-05 11:53:58 +0800

    Update game.h

--- game.h
+++ game.h
@@ -1,8 +1,9 @@
 class Game{
   public:
-    void start_game();//开始游戏
-    void select();//选择模式
-    void oneplayer();//单人模式
-    void twoplayers();//双人模式
+    void YNmove();//检测方块是否可以移动，与别的方块或者界面的碰撞
+    int xc();//消除，并加分
+    void addblock();//增加一行带空格的方块
+    int pd();//判断是否到底
+    void restartgame();//将分数清零，胜方胜场加一
   private:
 };
