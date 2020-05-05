5b303939645f1082bb945eb42295d404244d4978
1355158565 <60255109+1355158565@users.noreply.github.com>
2020-05-05 11:57:39 +0800

    Update Player.h

--- Player.h
+++ Player.h
@@ -4,10 +4,6 @@ class Player{
     void pause();//暂停游戏
     void continue();//继续游戏
     void setcolor();//设置颜色
-    void addblock();//增加一行带空格的方块
-    int xc();//消除，并加分
-    int pd();//判断是否到底
-    void YNmove();//检测方块是否可以移动，与别的方块或者界面的碰撞
     void leftmove();//左移方块
     void rightmove();//右移方块
     void downmove();//加速方块下落
