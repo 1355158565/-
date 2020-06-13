24c4d9f4eca73965fa165595a260a01364d2e369
1355158565 <60255109+1355158565@users.noreply.github.com>
2020-06-13 13:13:29 +0800

    Update game.h

--- game.h
+++ game.h
@@ -1,9 +1,22 @@
-class Game{
-  public:
-    void YNmove();//检测方块是否可以移动，与别的方块或者界面的碰撞
-    int xc();//消除，并加分
-    void addblock();//增加一行带空格的方块
-    int pd();//判断是否到底
-    void restartgame();//将分数清零，胜方胜场加一
-  private:
+#include"block.h"
+#include"draw.h"
+class game :public block, public draw 
+{
+protected:
+    int space_c;//旋转次数
+    int nn;
+    int space_c1; 
+    int nnn;
+public:
+    game(int v, int vv);
+    void start_game1(); //开始游戏
+    void start_game2();
+    int xc();
+    int xc1();
+    int pd(int n, int space_c, int x, int y); //消除
+    int pd1(int n, int space_c, int x, int y);
+    void draw_kong(int base, int space_c, int a, int b); //画空方块
+    void draw_block(int base, int space_c, int a, int b); //画方块
+    void addline(struct Face* t); //增加垃圾行
+    void addline1(struct Face* t);
 };
